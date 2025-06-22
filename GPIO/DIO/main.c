88f35e94d06b6843f7ba56840613a633495b0fd2
nghiado06 <dungnhannghia2605@gmail.com>
2025-06-22 20:41:20 +0700

    Update

--- GPIO/DIO/main.c
+++ GPIO/DIO/main.c
@@ -1,33 +1,24 @@
-#define PERIPH_BASE 0x40000000UL
-#define APB2PERIPH_BASE (PERIPH_BASE + 0x10000)
-#define RCC_BASE (PERIPH_BASE + 0x21000)
-#define GPIOC_BASE (APB2PERIPH_BASE + 0x1000)
-
-#define RCC_APB2ENR (*(volatile unsigned int *)(RCC_BASE + 0x18))
-#define GPIOC_CRH (*(volatile unsigned int *)(GPIOC_BASE + 0x04))
-#define GPIOC_ODR (*(volatile unsigned int *)(GPIOC_BASE + 0x0C))
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_tim.h"
 
 void delay(volatile int t)
 {
-    for (int i = 0; i < t; i++)
-        ;
 }
 
 int main(void)
 {
-    // Enable GPIOC clock (IOPCEN bit = 1 << 4)
-    RCC_APB2ENR |= (1 << 4);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-    // Configure PC13 as General purpose output push-pull, max speed 2 MHz (0b0010)
-    GPIOC_CRH &= ~(0xF << 20); // Clear bits 20-23 (PC13)
-    GPIOC_CRH |= (0x2 << 20);  // MODE13 = 0b10 (output 2MHz), CNF13 = 0b00 (push-pull)
+    GPIO_InitTypeDef Gpio;
+    Gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+    Gpio.GPIO_Pin = GPIO_Pin_13;
+    Gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &Gpio);
 
     while (1)
     {
-        GPIOC_ODR |= (1 << 13); // LED ON (active-low)
-        delay(2000000);
-
-        GPIOC_ODR &= ~(1 << 13); // LED OFF
-        delay(2000000);
+        GPIO_ResetBits(GPIOC, GPIO_Pin_13);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Automative-ECU-Development
-Learning
+- Đây sẽ là nội dung kiến thức mình học được và chia sẻ lại cho các bạn về lập trình trên dòng vi điều khiển STM32F1x
+- Chúng ta sẽ cùng tìm hiểu về lập trình thanh ghi (regis) và lập trình theo hướng sử dụng SPL (Standard Peripheral Library).
+- Bên cạnh đó, chúng ta sẽ cùng tìm hiểu về AUTOSAR Classic, các tiêu chuẩn, quy tắc, code của AUTOSAR.
